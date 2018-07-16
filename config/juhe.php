8c73297cc9ebc1a8eb599ae023596b39c55258d6
v_xuhuanju <v_xuhuanju@baidu.com>
2018-07-16 14:14:02 +0800

    add juhe api

---
+++ config/juhe.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v_xuhuanju
+ * Date: 2018/7/16
+ * Time: 13:42
+ */
+
+return [
+    "keys" => [
+        "news" => env("JUHE_NEWS_KEY", "your-key"),
+    ]
+];
